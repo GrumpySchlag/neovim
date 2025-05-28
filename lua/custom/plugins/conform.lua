965b67948d55d3689fa38283444a570e96a7e6e5
Kevin Notari <kevin.notari@etat.ge.ch>
2025-05-28 11:09:56 +0200

    fix php prettier

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -14,7 +14,7 @@ return {
         json = { 'prettierd', 'prettier' },
         json5 = { 'prettierd', 'prettier' },
         lua = { 'stylua' },
-        php = { 'phpcbf', 'php_cs_fixer', stop_after_first = true },
+        php = { 'php_cs_fixer', 'phpcbf' },
         python = { 'isort', 'black' },
         rust = { 'rustfmt' },
         twig = { 'twig-cs-fixer', 'djlint' },
